f76b7bd06142bc4d7c80641c3b5a54d3986b9808
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-07-16 16:16:10 -0300

    df

--- resources/views/paginas/home/home_contacto_formulario.blade.php
+++ resources/views/paginas/home/home_contacto_formulario.blade.php
@@ -5,25 +5,35 @@
                             'id'       => 'Form-LogIn'
                             ])               !!}
 
-            <div class="Auth-Field-Contenedor">
-                  {!! Form::text('email', null ,
-                    ['class'              => 'Auth-Field-Contenedor-Label-Input',
-                     'required'           => 'required',
-                     'data-pattern'       => 'calle',
-                     'data-empty-message' => 'Ingresa Tu Email',
-                     'data-error-message' => 'Escribe Un Email',
-                     'placeholder'        => 'Correo']) !!}
-             </div>
-
-              <div class="Auth-Field-Contenedor">
-              {!! Form::password('password', [ 'class'              => 'Auth-Field-Contenedor-Label-Input',
-                     'required'           => 'required',                     
-                     'placeholder'        => 'password']) !!}
-              </div>
-
-
-
-              <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Enviar</a>
+               <div class="form-group">
+                    <label class="control-label" for="Nombre">Nombres</label>
+                    <input type="text" class="form-control" id="Nombre" name="Nombre" placeholder="Introduzca su nombre" required autofocus />
+                </div>            
+                <div class="form-group">
+                    <label class="control-label" for="Motivo">Motivo de Contacto</label>
+                    <select name="Motivo" class="form-control">
+                        <option value="Consulta General">Consulta General</option>
+                        <option value="Realizar Pedido">Realizar Pedido</option>
+                        <option value="Informe un problema">Informe un problema</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="Empresa">Empresa</label>
+                    <input type="text" class="form-control" id="Empresa" name="Empresa" placeholder="Introduzca el nombre de su empresa" required />
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="Correo">Dirección de Correo Electrónico</label>
+                    <input type="email" class="form-control" id="Correo" name="Correo" placeholder="Introduzca su correo electrónico" required />
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="Mensaje">Mensaje</label>
+                    <textarea rows="5" cols="30" class="form-control" id="Mensaje" name="Mensaje" placeholder="Introduzca su mensaje" required ></textarea>
+                </div>
+                <div class="form-group">                
+                    <input type="submit" class="btn btn-primary" value="Enviar">
+                    <input type="reset" class="btn btn-default" value="Limpiar">                
+                </div>
+                <div id="respuesta" style="display: none;"></div>
 
 
 
