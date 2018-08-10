7a1d07161b6b994fbdb396631f98d0fc875ea414
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-10 11:05:37 -0300

    fg

--- resources/views/paginas/home/home.blade.php
+++ resources/views/paginas/home/home.blade.php
@@ -12,7 +12,7 @@
 
     {{-- css --}}
     <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/creative_template_mixer.css') }}"> 
-    <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/app.css') }}">     
+    <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/mixer.css') }}">     
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
