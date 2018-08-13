dc11ed842e238509e3b45bd5d90c127af8e0a9ae
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-13 16:18:21 -0300

    gh

--- resources/views/paginas/home/home.blade.php
+++ resources/views/paginas/home/home.blade.php
@@ -107,10 +107,10 @@ de memorables experiencias.
     <section class="bg-primary" id="morethanfishing">
       <div class="container">
         <div class="row">
-          <div class="">
-            <img class="col-lg-4 mx-auto text-center" src="{{url()}}/imagenes/{{$Empresa->logo_horizontal}}">
+          <div class="col-lg-4 mx-auto text-center">
+            <img class="" src="{{url()}}/imagenes/{{$Empresa->logo_horizontal}}">
           </div>
-          <div class="col-lg-8 mx-auto text-center">
+          <div class="col-lg-8 mx-auto ">
             <h2 class="section-heading text-white">#MORETHANFISHING</h2>
             <hr class="light my-4">
             <p class="text-faded mb-4">Ofrecemos una experiencia enriquecida para pescadores deportivos que gustan de una pesca dinámica y entretenida en sus diversos modos. Procuramos explorar constantemente nuestras Ríos y Arroyos ofreciendo las mejores locaciones en cuanto a belleza, estructura y la calidad de la pesca.
