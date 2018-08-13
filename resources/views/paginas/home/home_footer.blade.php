05f4e036e97302c807c4737e28144227033ddeae
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-13 17:27:33 -0300

    fg

---
+++ resources/views/paginas/home/home_footer.blade.php
@@ -0,0 +1,23 @@
+ <div class="container" style="background-color:#30302f;">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 text-center">
+            <img src="">
+          </div>
+          <div class="col-lg-4 col-md-4 text-center">
+            <p class="color-text-gris">2601 1234</p>
+            <p class="color-text-gris">099 123 456</p>
+            <p class="color-text-gris">contacto@uruwild.com</p>
+          </div> 
+          <div class="col-lg-4 col-md-4 text-center">
+              <p class="text-muted mb-0 color-text-gris">URUWILD 2018</p>
+              <p class="color-text-gris">Fotografía: Pablo Donagaray</p>
+              <p class="color-text-gris">Fotografía: Pablo DonagarayDiseño web: <a href="">Simbionte</a> </p>
+
+              <div class="mx-auto mt-4">
+              <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-facebook"></span></a></span>
+              <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-instagram"></span></a></span>
+              <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-twitter"></span></a></span>
+              </div>
+          </div>          
+        </div>
+</div>
