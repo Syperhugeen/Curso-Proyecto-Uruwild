6d054b7422e712782cdd7848810f6eb909d0284b
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-14 17:51:24 -0300

    fg

--- resources/views/paginas/home/slider.blade.php
+++ resources/views/paginas/home/slider.blade.php
@@ -1,5 +1,42 @@
 <div  class="{{-- masthead --}} get_width_100 mostrar-cabecera-destop"  >
-  <div id="slider_home">
+  
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+  {{-- <div id="slider_home">
 
     <li class="get_width_100  text-center text-white d-flex " style="position: relative;">
       <img src="{{url()}}/imagenes/Slider/uruwild_portada_foto1.jpg" style="height: 100%;width: 100%; ">
@@ -25,7 +62,7 @@
       <img src="{{url()}}/imagenes/Slider/uruwild_portada_foto5.jpg" style="height: 100%;width: 100%; ">
       @include('paginas.home.slider_texto_present')
     </li>
-  </div>
+  </div> --}}
 </div>
 
 <div class="get_width_100 ocultar-cabecera-mobil">
