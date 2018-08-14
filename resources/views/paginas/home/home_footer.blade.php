58bdba5c2dfea0f524417721d9d963a584608e93
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-14 13:39:34 -0300

    fg

--- resources/views/paginas/home/home_footer.blade.php
+++ resources/views/paginas/home/home_footer.blade.php
@@ -1,12 +1,15 @@
  <section  style="background-color:#30302f;">
+        <div class="container">
+          
+        
         <div class="row">
            <div class="col-lg-4 col-md-4 text-center">
             <img src="{{url()}}/imagenes/Empresa/uruwild_logo_white.png" class="get_width_100">
           </div>
-          <div class="col-lg-4 col-md-4 text-center">
-            <p class="color-text-gris"> <i class="fa fa-phone"></i>2601 1234</p>
-            <p class="color-text-gris"> <i class="fa fa-phone-square"></i> 099 123 456</p>
-            <p class="color-text-gris"><i class="fa fa-envelope"></i> contacto@uruwild.com</p>
+          <div class="col-lg-4 col-md-4 ">
+            <span class="color-text-gris"> <i class="fa fa-phone"></i> 2601 1234</span>
+            <span class="color-text-gris"> <i class="fa fa-phone-square"></i> 099 123 456</span>
+            <span class="color-text-gris"><i class="fa fa-envelope"></i> contacto@uruwild.com</span>
           </div> 
           <div class="col-lg-4 col-md-4 text-center">
               <p class="text-muted mb-0 color-text-gris">URUWILD 2018</p>
@@ -20,6 +23,7 @@
               </div>
           </div>          
         
+        </div>
         </div>
          
 </section>
