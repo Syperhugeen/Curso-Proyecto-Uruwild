5634f88f3f50bd0cabd7f9a0139c6c651f93bac5
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-14 18:47:37 -0300

    fg

--- resources/views/paginas/home/slider.blade.php
+++ resources/views/paginas/home/slider.blade.php
@@ -18,14 +18,7 @@
       <img class="d-block w-100" src="{{url()}}/imagenes/Slider/uruwild_portada_foto3.jpg" alt="Third slide">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+ 
 </div>
 
 
