1da43a7e6b2b7f4159cede50859dc8670967d7bb
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-14 15:42:16 -0300

    fg

--- resources/views/paginas/home/slider.blade.php
+++ resources/views/paginas/home/slider.blade.php
@@ -10,5 +10,24 @@
 
 
  <header class="masthead text-center text-white d-flex">
-      @include('paginas.home.slider_texto_present')
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong>¿TE APASIONAN LA NATURALEZA, EL ECOTURISMO Y LA PESCA?</strong>
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5">Te invitamos a vivir una experiencia que te hará renacer.</p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">¡DAME DOS!</a>
+          </div>
+          
+          <div class="col-md-6 mx-auto mt-4">
+              <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-facebook"></span></a></span>
+              <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-instagram"></span></a></span>
+              <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-twitter"></span></a></span>
+          </div>
+        </div>
+      </div>
     </header>
