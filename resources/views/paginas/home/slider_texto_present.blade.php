3daf5b1579ad0228e84abe79502ea0465c983ab4
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-14 14:42:31 -0300

    gh

--- resources/views/paginas/home/slider_texto_present.blade.php
+++ resources/views/paginas/home/slider_texto_present.blade.php
@@ -1,6 +1,6 @@
   <div class="my-auto get_width_100"  style="height: 100%; position:absolute;top:0;left:0;">
-        <div style="height:100%;" class="contenedor-baner-img-tex-html">
-        <div >
+        <div style="height:100%;" class="contenedor-baner-img-tex-html container ">
+        <div class="row">
           <div class="col-lg-10 mx-auto">
             <h1 class="text-uppercase">
               <strong>¿Te apasionaN la naturaleza, el ecoturismo y la pesca?</strong>
