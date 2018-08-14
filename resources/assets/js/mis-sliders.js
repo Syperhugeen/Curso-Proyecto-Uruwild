4b4e62fe18fb577ff53e0177c2a6e31caf71081f
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-14 17:13:59 -0300

    fg

--- resources/assets/js/mis-sliders.js
+++ resources/assets/js/mis-sliders.js
@@ -2,25 +2,10 @@
 
 $('#slider_home').slippry({
 
-  
-  
-   // options
-  adaptiveHeight: false, // height of the sliders adapts to current slide
-  start: 'random', // num (starting from 1), random
- 
-  captionsSrc: 'li',
-  captions: 'custom', // Position: overlay, below, custom, false
-  
+  pager:false,
+  controls:false
 
-  // transitions
-  transition: 'fade', // fade, horizontal, kenburns, false
-  easing: 'linear', // easing to use in the animation [(see... [jquery www])]
   
-
-  // slideshow
-  auto: false
-
-
 
 
 });
