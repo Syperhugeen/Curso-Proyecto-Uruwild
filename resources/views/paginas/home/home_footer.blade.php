f5a799483d31d3836bbbcbcf271a6fe4a5047853
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-14 13:48:05 -0300

    sd

--- resources/views/paginas/home/home_footer.blade.php
+++ resources/views/paginas/home/home_footer.blade.php
@@ -3,16 +3,16 @@
           
         
         <div class="row">
-           <div class="col-lg-4 col-md-4 text-center">
+           <div class="col-lg-4 col-md-4 text-center padding-lg">
             <img src="{{url()}}/imagenes/Empresa/uruwild_logo_white.png" class="get_width_100">
           </div>
-          <div class="col-lg-4 col-md-4 ">
-            <span class="color-text-gris"> <i class="fa fa-phone"></i> 2601 1234</span>
-            <span class="color-text-gris"> <i class="fa fa-phone-square"></i> 099 123 456</span>
-            <span class="color-text-gris"><i class="fa fa-envelope"></i> contacto@uruwild.com</span>
+          <div class="col-lg-4 col-md-4 padding-lg">
+            <p class="color-text-gris"> <i class="fa fa-phone"></i> 2601 1234</p>
+            <p class="color-text-gris"> <i class="fa fa-phone-square"></i> 099 123 456</p>
+            <p class="color-text-gris"><i class="fa fa-envelope"></i> contacto@uruwild.com</p>
           </div> 
-          <div class="col-lg-4 col-md-4 text-center">
-              <p class="text-muted mb-0 color-text-gris">URUWILD 2018</p>
+          <div class="col-lg-4 col-md-4 text-center padding-lg">
+              <p class="color-text-gris">URUWILD 2018</p>
               <p class="color-text-gris">Fotografía: Pablo Donagaray</p>
               <p class="color-text-gris">Fotografía: Pablo DonagarayDiseño web: <a href="">Simbionte</a> </p>
 
