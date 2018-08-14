1c7689eebf4f97c86f39c087859816a4361e1db3
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-14 14:24:15 -0300

    ok

--- resources/views/paginas/home/home_footer.blade.php
+++ resources/views/paginas/home/home_footer.blade.php
@@ -1,29 +1,27 @@
- <section  style="background-color:#30302f;">
-        <div class="container">
-          
-        
-        <div class="row">
-           <div class="col-lg-4 col-md-4 text-center padding-xl">
-            <img src="{{url()}}/imagenes/Empresa/uruwild_logo_white.png" class="get_width_100">
-          </div>
-          <div class="col-lg-4 col-md-4 text-center padding-xl">
-            <p class="color-text-gris small"> <i class="fa fa-phone"></i> 2601 1234</p>
-            <p class="color-text-gris small"> <i class="fa fa-phone-square"></i> 099 123 456</p>
-            <p class="color-text-gris small"><i class="fa fa-envelope"></i> contacto@uruwild.com</p>
-          </div> 
-          <div class="col-lg-4 col-md-4 text-center padding-xl">
-              <p class="color-text-gris small">URUWILD 2018</p>
-              <p class="color-text-gris small">Fotografía: Pablo Donagaray</p>
-              <p class="color-text-gris small">Fotografía: Pablo DonagarayDiseño web: <a href="">Simbionte</a> </p>
+ <section class="padding-xl" style="background-color:#30302f;">
+       <div class="container">
+          <div class="row">
+             <div class="col-lg-4 col-md-4 text-center padding-xl">
+              <img src="{{url()}}/imagenes/Empresa/uruwild_logo_white.png" class="get_width_100">
+            </div>
+            <div class="col-lg-4 col-md-4 text-center padding-xl">
+              <p class="color-text-gris small"> <i class="fa fa-phone"></i> 2601 1234</p>
+              <p class="color-text-gris small"> <i class="fa fa-phone-square"></i> 099 123 456</p>
+              <p class="color-text-gris small"><i class="fa fa-envelope"></i> contacto@uruwild.com</p>
+            </div> 
+            <div class="col-lg-4 col-md-4 text-center padding-xl">
+                <p class="color-text-gris small">URUWILD 2018</p>
+                <p class="color-text-gris small">Fotografía: Pablo Donagaray</p>
+                <p class="color-text-gris small">Diseño web: <a href="">Simbionte</a> </p>
 
-              <div class="mx-auto mt-4">
-              <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-facebook"></span></a></span>
-              <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-instagram"></span></a></span>
-              <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-twitter"></span></a></span>
-              </div>
-          </div>          
-        
-        </div>
+                <div class="mx-auto mt-4">
+                <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-facebook"></span></a></span>
+                <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-instagram"></span></a></span>
+                <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-twitter"></span></a></span>
+                </div>
+            </div>          
+          
+          </div>
         </div>
          
 </section>
