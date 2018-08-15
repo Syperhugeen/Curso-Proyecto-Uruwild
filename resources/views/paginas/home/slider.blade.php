1a86aa895785a9ac56e41b490bd8ac953972a20a
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-15 17:04:26 -0300

    jk

--- resources/views/paginas/home/slider.blade.php
+++ resources/views/paginas/home/slider.blade.php
@@ -10,7 +10,7 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src="{{url()}}/imagenes/Slider/uruwild_portada_foto1.jpg" alt="First slide">
-      <div class="carousel-caption d-none d-md-block">
+      <div class="carousel-caption d-none d-md-block Helper-OrdenarHijos-Row">
         @include('paginas.home.datos_para_carousel')
       </div>
     </div>
