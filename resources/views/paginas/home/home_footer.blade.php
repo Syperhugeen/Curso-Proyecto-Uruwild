f7ddf59ad623f3d27625a844ed4e8bf4e3354ec6
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-16 19:09:54 -0300

    df

--- resources/views/paginas/home/home_footer.blade.php
+++ resources/views/paginas/home/home_footer.blade.php
@@ -4,23 +4,29 @@
              <div class="col-lg-4 col-md-4 text-center padding-xl Helper-OrdenarHijos-Row">
               <img src="{{url()}}/imagenes/Empresa/uruwild_logo_white.png" class="get_width_100 p-5">
             </div>
-            <div class="col-lg-4 col-md-4 text-center padding-xl border-switch Helper-OrdenarHijos-Row">
-              <p class="color-text-gris espacio-letras small"> <i class="fa fa-phone"></i> 2601 1234</p>
+            <div class="col-lg-4 col-md-4 text-center padding-xl border-switch ">
+              <div class="Helper-OrdenarHijos-Row">
+               <p class="color-text-gris espacio-letras small"> <i class="fa fa-phone"></i> 2601 1234</p>
               <p class="color-text-gris espacio-letras small"> <i class="fa fa-phone-square"></i> 099 123 456</p>
               <p class="color-text-gris espacio-letras small"><i class="fa fa-envelope"></i> contacto@uruwild.com</p>
-
+             
                <div class="mx-auto mt-4">
                 <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-facebook"></span></a></span>
                 <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-instagram"></span></a></span>
                 <span class="col-md-4 mx-auto"><a href=""><span class="socicon socicon-twitter"></span></a></span>
                 </div>
+
+              </div>
+              
+
+              
             </div> 
-            <div class="col-lg-4 col-md-4 text-center padding-xl border-switch Helper-OrdenarHijos-Row">
+            <div class="col-lg-4 col-md-4 text-center padding-xl border-switch">
+              <div class="Helper-OrdenarHijos-Row">
                 <p class="color-text-gris espacio-letras small">URUWILD 2018</p>
                 <p class="color-text-gris espacio-letras small">Fotografía: Pablo Donagaray</p>
                 <p class="color-text-gris espacio-letras small">Diseño web: <a href="">Simbionte</a> </p>
-
-               
+              </div>
             </div>          
           
           </div>
