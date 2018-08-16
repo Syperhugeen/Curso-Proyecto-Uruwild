9144f6d86a5acd87001e3ab8921031aef0ca6a2a
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-16 15:44:40 -0300

    gh

--- resources/views/paginas/home/home_actividades.blade.php
+++ resources/views/paginas/home/home_actividades.blade.php
@@ -12,14 +12,14 @@
         <div class="row">
           <div class="col-lg-6 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
-              <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i>
+              <img class="iconos-custom" src="{{url()}}/imagenes/Iconos/uruwild_icon_pesca.png">
               <h3 class="mb-3">Pesca</h3>
               <p class="text-muted mb-0">¡Vení a vivir la pesca deportiva de una manera diferente! Aguas de Vadeo, float tubes y canoas. Te acercamos a los mejores ríos, arroyos y cañadas de agua clara para perseguir a pez visto las increíbles tarariras tornasol y los esafiantes dorados del Uruguay.</p>
             </div>
           </div>
           <div class="col-lg-6 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
-              <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
+               <img class="iconos-custom" src="{{url()}}/imagenes/Iconos/uruwild_icon_ecoturismo.png">
               <h3 class="mb-3">Ecoturismo</h3>
               <p class="text-muted mb-0">Paseos en canoa y en bici, cabalgatas, caminatas y senderos, birdwatching y fotografía, reconocimiento de flora y fauna, o simplemente disfrutar de la tranquilidad de la naturaleza como más te guste.</p>
             </div>
--- resources/views/paginas/home/home_services.blade.php
+++ resources/views/paginas/home/home_services.blade.php
@@ -12,21 +12,21 @@
         <div class="row">
           <div class="col-lg-4 col-md-4 text-center">
             <div class="service-box mt-5 mx-auto">
-              <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i>
+              <img class="iconos-custom" src="{{url()}}/imagenes/Iconos/uruwild_icon_estadia.png">
               <h3 class="mb-3">Estadía</h3>
               <p class="text-muted mb-0">Hospedaje en estancias de lujo, campamentos a orillas del río o pequeños ranchos. Gastronomía y comida local, refrescos y bebidas.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-4 text-center">
             <div class="service-box mt-5 mx-auto">
-              <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
+               <img class="iconos-custom" src="{{url()}}/imagenes/Iconos/uruwild_icon_traslados.png">
               <h3 class="mb-3">Traslados</h3>
               <p class="text-muted mb-0">Ofrecemos Traslados y desplazamientos por tierra o aire..</p>
             </div>
           </div> 
              <div class="col-lg-4 col-md-4 text-center">
             <div class="service-box mt-5 mx-auto">
-              <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
+              <img class="iconos-custom" src="{{url()}}/imagenes/Iconos/uruwild_icon_fotografia.png">
               <h3 class="mb-3">Plus Incluído</h3>
               <p class="text-muted mb-0">Guías especializados para las jornadas y servicio de fotografía incluído para registrar tu estadía.</p>
             </div>
