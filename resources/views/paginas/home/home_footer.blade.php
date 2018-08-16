e68131bda04e9b6f67e9b2f7ed9942b50d8f3e5a
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-16 19:13:08 -0300

    sd

--- resources/views/paginas/home/home_footer.blade.php
+++ resources/views/paginas/home/home_footer.blade.php
@@ -5,7 +5,7 @@
               <img src="{{url()}}/imagenes/Empresa/uruwild_logo_white.png" class="get_width_100 p-5">
             </div>
             <div class="col-lg-4 col-md-4 text-center padding-xl border-switch Helper-OrdenarHijos-Row">
-              <div class="">
+              <div class="get_width_100">
                <p class="color-text-gris espacio-letras small"> <i class="fa fa-phone"></i> 2601 1234</p>
               <p class="color-text-gris espacio-letras small"> <i class="fa fa-phone-square"></i> 099 123 456</p>
               <p class="color-text-gris espacio-letras small"><i class="fa fa-envelope"></i> contacto@uruwild.com</p>
@@ -22,7 +22,7 @@
               
             </div> 
             <div class="col-lg-4 col-md-4 text-center padding-xl border-switch Helper-OrdenarHijos-Row">
-              <div class="">
+              <div class="get_width_100">
                 <p class="color-text-gris espacio-letras small">URUWILD&#x24B8;2018</p>
                 <p class="color-text-gris espacio-letras small">Fotografía: Pablo Donagaray</p>
                 <p class="color-text-gris espacio-letras small">Diseño web: <a href="">Simbionte</a> </p>
