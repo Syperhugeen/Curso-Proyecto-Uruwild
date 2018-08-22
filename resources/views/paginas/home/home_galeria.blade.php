ad0106062ff8548d166fa72b702cd3631c44da16
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-08-22 13:45:21 -0300

    df

--- resources/views/paginas/home/home_galeria.blade.php
+++ resources/views/paginas/home/home_galeria.blade.php
@@ -106,22 +106,7 @@
                 </div>
               </div>
             </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/Lugares preservados.jpg">
-              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/Lugares preservados.jpg" alt="">
-              <div class="portfolio-box-caption">
-                <div class="portfolio-box-caption-content">
-                  <div class="project-category text-faded">
-                    Lugares preservados
-                  </div>
-                  <div class="project-name">
-                    Lugares preservados
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
+          </div>         
           <div class="col-lg-4 col-sm-6">
              <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/Birdwatching!.jpg">
               <img class="img-fluid" src="{{url()}}/imagenes/Galeria/Birdwatching!.jpg" alt="">
