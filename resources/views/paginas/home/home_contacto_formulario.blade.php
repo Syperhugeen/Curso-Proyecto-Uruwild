05fcbc2a345ca59dc48be61c09a557e35739a976
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-09-03 17:14:24 -0300

    df

--- resources/views/paginas/home/home_contacto_formulario.blade.php
+++ resources/views/paginas/home/home_contacto_formulario.blade.php
@@ -31,7 +31,7 @@
                 </div>
                 <div class="form-group" v-show="nombre !='' && email !='' && mensaje !='' ">                
                     <div v-on:click="enviar_contacto" class="btn btn-primary" value="Enviar"> Enviar </div>
-                    <input type="submit" class="btn btn-primary" value="Enviar Submit"> 
+                    {{-- <input type="submit" class="btn btn-primary" value="Enviar Submit">  --}}
                 </div>
                 <div id="respuesta" style="display: none;"></div>
 
