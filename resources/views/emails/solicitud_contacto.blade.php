2e739dc92821b320306bb842b020eb1793150a08
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-09-07 14:21:01 -0300

    fg

--- resources/views/emails/solicitud_contacto.blade.php
+++ resources/views/emails/solicitud_contacto.blade.php
@@ -1,12 +1,21 @@
+
+<div style="width:500px;">
+  
+
+<br>
+<div style="text-align: center;">
+  Solicitud de contacto de <strong>{{$nombre}} </strong>({{$email}})
+</div>
+
+
+<br>
+<br>
 <br>
 
-Solicitud de contacto de {{$nombre}} ({{$email}})
-
-<br>
-
-<div style="padding: 10px; border-radius: 4px; background-color: #f8f8f8;" >
+<div style="padding: 10px; border-radius: 4px; background-color: #f8f8f8; text-align: center;" >
   {{$mensaje}}
 </div>
 
 <br>
+</div>
 
