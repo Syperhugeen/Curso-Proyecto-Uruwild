bfaeb719db6c2acb58f6cdb11b2269c7ddd7da64
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-09-11 11:09:46 -0300

    ok

--- resources/views/paginas/home/home_morethanfishing.blade.php
+++ resources/views/paginas/home/home_morethanfishing.blade.php
@@ -2,11 +2,14 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-4 mx-auto text-center padding-xl">
-            <img class="get_width_70" src="{{url()}}/imagenes/Empresa/uruwild_logo_white.png">
+            <div class="get_width_100 flex-row-center flex-justifice-space-around">
+              <img class="logo-MORETHANFISHING" src="{{url()}}/imagenes/Empresa/uruwild_logo_white.png">
+            </div>
+            
           </div>
           <div class="col-lg-8 mx-auto padding-xl">
-            <h2 class="section-heading text-white  texto-centrado-personalizado">#MORETHANFISHING</h2>            
-            <p class="text-white mb-4 texto-centrado-personalizado">Ofrecemos una experiencia innovadora y diferente en ecoturismo y en pesca deportiva. Exploramos constantemente nuestros ríos y arroyos para conocer y acercarte a las mejores locaciones, teniendo en cuenta su belleza paisajística, su biodiversidad, y la posibilidad de tener una experiencia altamente dinámica e interactiva con el entorno natural.
+            <h2 class="section-heading text-white  texto-MORETHANFISHING">#MORETHANFISHING</h2>            
+            <p class="text-white mb-4 texto-MORETHANFISHING">Ofrecemos una experiencia innovadora y diferente en ecoturismo y en pesca deportiva. Exploramos constantemente nuestros ríos y arroyos para conocer y acercarte a las mejores locaciones, teniendo en cuenta su belleza paisajística, su biodiversidad, y la posibilidad de tener una experiencia altamente dinámica e interactiva con el entorno natural.
             </p>
             
           </div>
