bce149210972f60900c451a9b21f6cfe951c147a
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-09-10 22:56:28 -0300

    df

--- resources/views/paginas/home/slider.blade.php
+++ resources/views/paginas/home/slider.blade.php
@@ -1,40 +1,21 @@
 
 <div  class="{{-- masthead --}} get_width_100 mostrar-cabecera-destop"  >
   
-<div id="carouselExampleIndicators" class="carousel slide auto" data-ride="carousel" data-interval="3000">
+<div id="carouselExampleIndicators" class="carousel slide auto" data-ride="carousel" data-interval="5000">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto1.jpg" alt="First slide">
-     
-      
+      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto1.jpg" alt="First slide"> 
+    </div>
+     <div class="carousel-item">
+      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto6b.jpg" alt="Third slide">
     </div>
     <div class="carousel-item">
       <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto2.jpg" alt="Second slide">
-       
-    </div>
-    <div class="carousel-item">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto3b.jpg" alt="Third slide">
-       
-    </div>
-    <div class="carousel-item">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto4b.jpg" alt="Third slide">
-       
-    </div>
-    <div class="carousel-item">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto5.jpg" alt="Third slide">
-       
-    </div>
-    <div class="carousel-item">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto6a.jpg" alt="Third slide">
-       
     </div>
   </div>
 
