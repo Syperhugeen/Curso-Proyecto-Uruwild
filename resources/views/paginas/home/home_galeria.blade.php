57bc950dc659b4465d8ba020b3bd1de6878f161f
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-09-19 13:32:19 -0300

    ok

--- resources/views/paginas/home/home_galeria.blade.php
+++ resources/views/paginas/home/home_galeria.blade.php
@@ -1,5 +1,194 @@
-    {{-- la experiencia Uruwild --}}
-    <section class="p-0" id="portfolio">
+    {{-- la experiencia Uruwild REPONSIVE--}}
+    <section class="p-0 mostrar_en_small" id="portfolio">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_arapey.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_arapey.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Río Arapey
+                  </div>
+                  <div class="project-name">
+                    Especial para pesca de Vadeo
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_bocking.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_bocking.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Estancia Arerungua
+                  </div>
+                  <div class="project-name">
+                     Un lugar para disfrutar
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_arroyo.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_arroyo.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Arroyo Sarandí
+                  </div>
+                  <div class="project-name">
+                    Rincones vírgenes y preservados
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_dorado.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_dorado.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Dorados a pez visto
+                  </div>
+                  <div class="project-name">
+                    Pesca de Vadeo
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_floattubeguayabos.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_floattubeguayabos.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Float Tubes
+                  </div>
+                  <div class="project-name">
+                    Una experiencia diferente
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_tararira.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_tararira.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Pesca de Tarariras
+                  </div>
+                  <div class="project-name">
+                    Arroyo Arerungua
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_urraca.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_urraca.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Urracas
+                  </div>
+                  <div class="project-name">
+                    Abundancia y variedad de aves
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>         
+          <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_coendu.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_coendu.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Coendú
+                  </div>
+                  <div class="project-name">
+                    Biodiversidad para disfrutar
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_martinpescador.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_martinpescador.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Martin Pescador
+                  </div>
+                  <div class="project-name">
+                    Birdwatching
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+           <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_nandu.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_nandu.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Ñandú
+                  </div>
+                  <div class="project-name">
+                    Un ave común de las praderas
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_quebradasdelnorte.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_quebradasdelnorte.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Cascadas y quebradas
+                  </div>
+                  <div class="project-name">
+                    Paisajes fascinantes del norte
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+             <a class="portfolio-box" href="{{url()}}/imagenes/Galeria/responsive/URUWILD_tacuarembo.jpg">
+              <img class="img-fluid" src="{{url()}}/imagenes/Galeria/responsive/URUWILD_tacuarembo.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Caminatas y senderos
+                  </div>
+                  <div class="project-name">
+                    Valle Edén
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+     {{-- la experiencia Uruwild NO-RESPONSIVE--}}
+    <section class="p-0 no_mostrar_en_small" id="portfolio">
       <div class="container-fluid p-0">
         <div class="row no-gutters popup-gallery">
           <div class="col-lg-4 col-sm-6">
