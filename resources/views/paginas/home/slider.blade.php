bee4bd081b3d8c9d3a5373ecf0fd489568dbf3f4
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-09-19 13:17:40 -0300

    ok

--- resources/views/paginas/home/slider.blade.php
+++ resources/views/paginas/home/slider.blade.php
@@ -7,28 +7,21 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner hidden-xs">
+  <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto1.jpg" alt="First slide"> 
+      <img class="img-slider-size Slider_cabecer_img" src="{{url()}}/imagenes/Slider/uruwild_portada_foto1.jpg" alt="First slide"> 
+      <img class="img-slider-size Slider_cabecer_img_responsive" src="{{url()}}/imagenes/Slider/responsive/uruwild_portada_foto1.jpg" alt="First slide">
     </div>
      <div class="carousel-item">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto6b.jpg" alt="Third slide">
+      <img class="img-slider-size Slider_cabecer_img" src="{{url()}}/imagenes/Slider/uruwild_portada_foto6b.jpg" alt="Third slide">
+      <img class="img-slider-size Slider_cabecer_img_responsive" src="{{url()}}/imagenes/Slider/responsive/uruwild_portada_foto6b.jpg" alt="Third slide">
     </div>
     <div class="carousel-item">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/uruwild_portada_foto2.jpg" alt="Second slide">
+      <img class="img-slider-size Slider_cabecer_img" src="{{url()}}/imagenes/Slider/uruwild_portada_foto2.jpg" alt="Second slide">
+      <img class="img-slider-size Slider_cabecer_img_responsive" src="{{url()}}/imagenes/Slider/responsive/uruwild_portada_foto2.jpg" alt="Second slide">
     </div>
   </div>
-  <div class="carousel-inner visible-xs">
-    <div class="carousel-item active">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/responsive/uruwild_portada_foto1.jpg" alt="First slide"> 
-    </div>
-     <div class="carousel-item">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/responsive/uruwild_portada_foto6b.jpg" alt="Third slide">
-    </div>
-    <div class="carousel-item">
-      <img class="img-slider-size" src="{{url()}}/imagenes/Slider/responsive/uruwild_portada_foto2.jpg" alt="Second slide">
-    </div>
-  </div>
+
 
       <div class="carousel-caption  d-md-block Helper-OrdenarHijos-Row">
         @include('paginas.home.datos_para_carousel')
