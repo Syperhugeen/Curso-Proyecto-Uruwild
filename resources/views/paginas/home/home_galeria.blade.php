0d80a68a7eb66439960da5436263841de8eb9439
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-09-24 12:03:34 -0300

    fg

--- resources/views/paginas/home/home_galeria.blade.php
+++ resources/views/paginas/home/home_galeria.blade.php
@@ -1,5 +1,6 @@
     {{-- la experiencia Uruwild REPONSIVE--}}
-    <section class="p-0 mostrar_en_small" id="galeria">
+    <span id="galeria"></span>
+    <section class="p-0 mostrar_en_small" id="">
       <div class="container-fluid p-0">
         <div class="row no-gutters popup-gallery">
           <div class="col-lg-4 col-sm-6">
@@ -188,7 +189,7 @@
 
 
      {{-- la experiencia Uruwild NO-RESPONSIVE--}}
-    <section class="p-0 no_mostrar_en_small" id="portfolio">
+    <section class="p-0 no_mostrar_en_small">
       <div class="container-fluid p-0">
         <div class="row no-gutters popup-gallery">
           <div class="col-lg-4 col-sm-6">
