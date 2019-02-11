e4fc727b908f2596ba235267604d5e968c0d1652
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-11 09:59:14 -0300

    df

--- resources/views/paginas/home/home.blade.php
+++ resources/views/paginas/home/home.blade.php
@@ -77,6 +77,16 @@
   <!-- End of SMOOTH SCROLL -->
 
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133958643-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-133958643-1');
+    </script>
+
+
 
   </head>
 
